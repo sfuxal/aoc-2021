ef2c91067f9b055e36165921aef1dd9482e3a1a7
Simone Fuchs <simone@getciara.com>
2021-12-24 14:19:01 -0300

    day 05 part 1

---
+++ src/days/05/index.ts
@@ -0,0 +1,99 @@
+import { toInt } from "../../lib/helpers";
+
+type Coordinate = {
+  x: number,
+  y: number,
+}
+
+type Line = {
+  start: Coordinate,
+  end: Coordinate
+}
+
+function toCoordinate(input: string): Coordinate {
+  const parts = input.split(',');
+  return {
+    x: toInt(parts[0]),
+    y: toInt(parts[1])
+  }
+}
+
+function formatInput(input: string[]): Line[] {
+  return input
+    .map((line) => {
+      const lineParts = line.split(' -> ');
+      const start = toCoordinate(lineParts[0]);
+      const end = toCoordinate(lineParts[1]);
+      return { start, end }
+    })
+}
+
+function checkDirection(line: Line): 'horizontal' | 'vertical' | undefined {
+  if (line.start.x === line.end.x) return 'vertical';
+  if (line.start.y === line.end.y) return 'horizontal';
+  return undefined;
+}
+
+function findStartEndIndex(start: number, end: number): number[] {
+  return [start, end].sort((a, b) => a - b);
+}
+
+function drawHorizontal(drawing: number[][], row: number, colStart: number, colEnd: number): number[][] {
+  for (let index = colStart; index <= colEnd; index++) {
+    if (!drawing[row]) drawing[row] = [];
+    if (drawing[row][index] == null) drawing[row][index] = 0;
+    drawing[row][index]++;
+  }
+  return drawing;
+}
+
+function drawVertical(drawing: number[][], column: number, rowStart: number, rowEnd: number): number[][] {
+  for (let index = rowStart; index <= rowEnd; index++) {
+    if (!drawing[index]) drawing[index] = [];
+    if (drawing[index][column] == null) drawing[index][column] = 0;
+    drawing[index][column]++;
+  }
+  return drawing;
+}
+
+function draw(lines: Line[]): number[][] {
+  let drawing: number[][] = [];
+
+  lines.forEach((line) => {
+    const direction = checkDirection(line);
+    if (!direction) return;
+
+    switch (direction) {
+      case 'horizontal':
+        const [colStart, colEnd] = findStartEndIndex(line.start.x, line.end.x);
+        const row = line.start.y;
+        drawing = drawHorizontal(drawing, row, colStart, colEnd);
+        break;
+      case 'vertical':
+        const [rowStart, rowEnd] = findStartEndIndex(line.start.y, line.end.y);
+        const column = line.start.x;
+        drawing = drawVertical(drawing, column, rowStart, rowEnd);
+        break;
+      default:
+        break;
+    }
+  })
+  return drawing;
+}
+
+export function one(_input: string[]): number {
+  const lines = formatInput(_input);
+  const drawing = draw(lines);
+  const count = drawing.reduce<number>((acc:number, row:number[]) => {
+    if(row == null) return acc;
+    return acc + row.reduce<number>((acc: number, value: number) => {
+      if(value >= 2) acc++;
+      return acc;
+    }, 0)
+  }, 0);
+  return count;
+}
+
+export function two(_input: string[]): number {
+  return 0;
+}
