c8d487245738928ef9411a86658cb483c3efcc0d
Simone Fuchs <simone@getciara.com>
2021-12-26 17:45:59 -0300

    day 07 part 2

--- src/days/07/index.ts
+++ src/days/07/index.ts
@@ -1,7 +1,12 @@
 import { toInt } from "../../lib/helpers";
 
 function getSortedFrequencies(locations: number[]): { location: number, frequency: number }[] {
-  const frequencies: Record<number, number> = {};
+  const maxLocation = Math.max(...locations);
+  //  start from 0
+  const frequencies: Record<number, number> = Array.from(Array(maxLocation + 1).keys()).reduce((acc:Record<number, number>, val:number) => {
+    acc[val] = 0;
+    return acc;
+  }, {});
   locations.forEach((location) => {
     if (!frequencies[location]) frequencies[location] = 1;
     else frequencies[location]++;
@@ -32,6 +37,28 @@ export function one(_input: string[]): number {
   return fuelCost;
 }
 
+function getCostForDistance(distance:number):number {
+  let cost = 0;
+  for (let index = distance; index > 0; index--) {
+    cost = cost + index;
+  }
+  return cost;
+}
+
+function getExponentialFuelCost(locations:number[], posToAlign:number):number {
+  return locations.reduce((acc:number, location) => {
+   return acc + getCostForDistance(Math.abs(location - posToAlign));
+ }, 0)
+}
+
 export function two(_input: string[]): number {
-  return 0;
+  const locations = _input[0].split(',').map((l) => toInt(l));
+  const sortedFrequencies = getSortedFrequencies(locations);
+  let fuelCost;
+  for (let index = 0; index < (sortedFrequencies.length / 2); index++) {
+    const cost = getExponentialFuelCost(locations, sortedFrequencies[index].location);
+    if(!fuelCost || cost < fuelCost) fuelCost = cost;
+  }
+  if(!fuelCost) throw new Error('Unexpected error');
+  return fuelCost;
 }
