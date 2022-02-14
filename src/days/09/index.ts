c1e34c37b881b9d8dbdba46a2c22d91aaea826e2
Simone Fuchs <simone@getciara.com>
2022-02-14 12:58:08 +0100

    day 09 part 2

--- src/days/09/index.ts
+++ src/days/09/index.ts
@@ -2,8 +2,18 @@ import { toInt } from "../../lib/helpers";
 
 type Coordinate = { rowIndex: number, colIndex: number }
 
+function getHeightMap(input: string[]): number[][] {
+  return input.map((line) => (
+    line.split('').map((height) => toInt(height)))
+  );
+}
+
 function getValueIfExists(nestedArray: number[][], rowIndex: number, colIndex: number) {
   return nestedArray[rowIndex] ? nestedArray[rowIndex][colIndex] : undefined
+}
+
+function getCoordinateString(rowIndex: number, colIndex: number) {
+  return `row:${rowIndex}-col:${colIndex}`
 }
 
 function findLowPointCoordinates(heightMap: number[][]): Coordinate[] {
@@ -23,10 +33,30 @@ function findLowPointCoordinates(heightMap: number[][]): Coordinate[] {
   return lowPointCoordinates;
 }
 
+function getHeightsAndCoordinates(heightMap: number[][], rowIndex: number, colIndex: number) {
+  const curCoordinate: string = getCoordinateString(rowIndex, colIndex);
+  const nextHeight: number | undefined = getValueIfExists(heightMap, rowIndex, colIndex + 1);
+  const nextCoordinate: string | undefined = nextHeight != null ? getCoordinateString(rowIndex, colIndex + 1) : undefined;
+  const downHeight: number | undefined = getValueIfExists(heightMap, rowIndex + 1, colIndex);
+  const downCoordinate: string | undefined = downHeight != null ? getCoordinateString(rowIndex + 1, colIndex) : undefined;
+  return {
+    curCoordinate, nextHeight, nextCoordinate, downHeight, downCoordinate
+  }
+}
+
+function findBasinIndexes(basins: string[][], curCoordinate: string, nextCoordinate: string | undefined) {
+  let curHeightBasinIndex = basins.findIndex((basin) => basin.includes(curCoordinate));
+  let nextHeightBasinIndex = -1;
+  if (nextCoordinate) {
+    nextHeightBasinIndex = curHeightBasinIndex !== -1 && basins[curHeightBasinIndex].includes(nextCoordinate)
+      ? curHeightBasinIndex
+      : basins.findIndex((basin) => basin.includes(nextCoordinate))
+  }
+  return { curHeightBasinIndex, nextHeightBasinIndex }
+}
+
 export function one(_input: string[]): number {
-  const heightMap = _input.map((line) => (
-    line.split('').map((height) => toInt(height)))
-  );
+  const heightMap = getHeightMap(_input);
 
   const lowPointCoordinates = findLowPointCoordinates(heightMap);
 
@@ -38,5 +68,51 @@ export function one(_input: string[]): number {
 }
 
 export function two(_input: string[]): number {
-  return 0;
+  const heightMap = getHeightMap(_input);
+
+  const basins: string[][] = [];
+
+  for (let rowIndex = 0; rowIndex < heightMap.length; rowIndex++) {
+    for (let colIndex = 0; colIndex < heightMap[0].length; colIndex++) {
+      const curHeight = heightMap[rowIndex][colIndex];
+
+      if (curHeight !== 9) {
+        const {
+          curCoordinate, nextHeight, nextCoordinate, downHeight, downCoordinate
+        } = getHeightsAndCoordinates(heightMap, rowIndex, colIndex);
+
+        let { curHeightBasinIndex, nextHeightBasinIndex } = findBasinIndexes(basins, curCoordinate, nextCoordinate);
+
+        // current and next height aren't in a basin yet
+        // -> start fresh basin
+        if (curHeightBasinIndex === -1 && nextHeightBasinIndex === -1) {
+          basins.push([curCoordinate]);
+        }
+        // current height is not in a basin yet but next one already is
+        // -> put current height in same basin as next height
+        if (curHeightBasinIndex === -1 && nextHeightBasinIndex !== -1) {
+          curHeightBasinIndex = nextHeightBasinIndex;
+          basins[curHeightBasinIndex].push(curCoordinate)
+        }
+        // current and next heights are in two different basins
+        // -> merge those basins basins
+        if (curHeightBasinIndex !== -1 && nextHeightBasinIndex !== -1 && curHeightBasinIndex !== nextHeightBasinIndex) {
+          basins[curHeightBasinIndex] = [...basins[curHeightBasinIndex], ...basins[nextHeightBasinIndex]];
+          basins.splice(nextHeightBasinIndex, 1);
+          if (nextHeightBasinIndex < curHeightBasinIndex) curHeightBasinIndex--;
+          nextHeightBasinIndex = curHeightBasinIndex;
+        }
+
+        // check and potentially add right / down neighbors
+        const curBasin = basins[(curHeightBasinIndex === -1 ? basins.length - 1 : curHeightBasinIndex)];
+        if (nextCoordinate && nextHeight !== 9 && nextHeightBasinIndex === -1) curBasin.push(nextCoordinate);
+        if (downCoordinate && downHeight !== 9) curBasin.push(downCoordinate);
+      }
+    }
+  }
+
+  basins.sort((a, b) => b.length - a.length);
+  return basins.slice(0, 3).reduce((acc, basin) => {
+    return acc * basin.length
+  }, 1)
 }
